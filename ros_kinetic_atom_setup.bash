baea76d575caf4ea119c01de8060a283d60c9420
Todd Sampson <tsampson@cloudspace.com>
2017-03-24 16:37:58 -0400

    Edit to rosbridge-server apt

--- ros_kinetic_atom_setup.bash
+++ ros_kinetic_atom_setup.bash
@@ -19,7 +19,7 @@ echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 echo $ROS_PACKAGE_PATH
 
-sudo apt-get install -y arduino ros-kinetic-rosserial-arduino ros-kinetic-rosserial ros-kinetic-rosserial-server ros-kinetc-rosbridge-server openssh-server vim
+sudo apt-get install -y arduino ros-kinetic-rosserial-arduino ros-kinetic-rosserial ros-kinetic-rosserial-server ros-kinetic-rosbridge-server openssh-server vim
 mkdir -p ~/sketchbook/libraries && cd ~/sketchbook/libraries && rosrun rosserial_arduino make_libraries.py .
 source ~/.bashrc
 
